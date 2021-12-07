fa8f3568b73012e62b5ffcfc8e2fbff778dbf80f
Tim Crooker <timothycrooker@gmail.com>
2021-12-07 17:21:17 -0500

    remove broken imports

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ export * from './merge'
 export * from './binary'
 export * from './cmd'
 export * from './files'
-export * from './git-user'
-export * from './glob'
 
 export const setLogLevel = (logLevel: LogLevel = 3): void => {
 	logger.setOptions({ logLevel })
